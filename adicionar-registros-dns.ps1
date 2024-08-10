11892863377822a121ad232b1f9695a35025c572
Leandro Franklin <leandrofranklin@msn.com>
2024-08-10 11:01:39 -0300

    Update adicionar-registros-dns.ps1

--- adicionar-registros-dns.ps1
+++ adicionar-registros-dns.ps1
@@ -1,3 +1,4 @@
+# Adicionar registros DNS para Management Domain
 Add-DnsServerResourceRecordA -Name "vcf-m01-cb01" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.2" -CreatePtr
 Add-DnsServerResourceRecordA -Name "vcf-m01-sddcm01" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.3" -CreatePtr
 Add-DnsServerResourceRecordA -Name "vcf-m01-vc01" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.4" -CreatePtr
@@ -11,3 +12,10 @@ Add-DnsServerResourceRecordA -Name "vcf-m01-esx05" -ZoneName "vmbeer.local" -IPv
 Add-DnsServerResourceRecordA -Name "vcf-m01-esx06" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.12" -CreatePtr
 Add-DnsServerResourceRecordA -Name "vcf-m01-esx07" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.13" -CreatePtr
 Add-DnsServerResourceRecordA -Name "vcf-m01-esx08" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.14" -CreatePtr
+
+# Adicionar registros DNS para Workload Domain
+Add-DnsServerResourceRecordA -Name "vcf-w01-vc01" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.15" -CreatePtr
+Add-DnsServerResourceRecordA -Name "vcf-w01-nsx01" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.16" -CreatePtr
+Add-DnsServerResourceRecordA -Name "vcf-w01-nsx01a" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.17" -CreatePtr
+Add-DnsServerResourceRecordA -Name "vcf-w01-nsx01b" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.18" -CreatePtr
+Add-DnsServerResourceRecordA -Name "vcf-w01-nsx01c" -ZoneName "vmbeer.local" -IPv4Address "192.168.68.19" -CreatePtr
